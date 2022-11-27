cc5067488b6d16a099cfc1b29654d195313b145b
SatXCho <92297743+SatXCho@users.noreply.github.com>
2022-11-27 22:02:29 +0530

    Add files via upload

---
+++ task-3/journeytojountelheim.py
@@ -0,0 +1,25 @@
+#portal 2 is a better game than god of war fight me
+
+def portal(x,list):
+    obtkeys = []
+    obtkeys.append(x)
+    obtkeys.append(list[x-1])
+    obtkeys.append(list[obtkeys[1]-1])
+    prod = 1
+    for i in obtkeys:
+        prod *= i
+    if prod == 6:
+        return "YES"
+    else:
+        return "NO"
+
+t = int(input())
+out = []
+for i in range(t):
+    x = int(input())
+    abc = list(map(int,input().split()))
+    out.append(portal(x,abc))
+
+for i in out:
+    print(i)
+
---
+++ task-3/losingstreak.py
@@ -0,0 +1,40 @@
+
+import math
+def primeFactors(n):
+	pf = []
+	while n % 2 == 0:
+		pf.append(2)
+		n = n / 2
+		
+	for i in range(3,int(math.sqrt(n))+1,2):
+		
+		while n % i== 0:
+			pf.append(i)
+			n = n / i
+			
+	if n > 2:
+		pf.append(n)
+
+	return pf
+
+
+
+
+monettt = list(map(int,input().split()))
+monet = monettt[1]
+t = monettt[0]
+k = []
+flag = 0
+for i in range(1, monet + 1):
+	if monet % i == 0 and i == t:
+		flag = 1
+		k.extend(primeFactors(monet//i))
+
+
+
+
+
+if all(el == 2 or el == 3 for el in k) and flag == 1:
+	print(len(k))
+else:
+	print(-1)
