18defdf008506871a2c5b1519757fbc08410bb5b
SatXCho <92297743+SatXCho@users.noreply.github.com>
2022-11-27 22:00:12 +0530

    wowza that's a lot of files

--- task-9/src/main.rs
+++ task-9/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     let document = scraper::Html::parse_document(&response);
     let crypto_name_selector = scraper::Selector::parse("div.css-ttxvk0>p").unwrap();
     let cryptoname = document.select(&crypto_name_selector).map(|x| x.inner_html());
-    println!("Name of the coin \n");
+    println!("\n NAME \n");
     cryptoname
         .zip(1..51)
         .for_each(|(item, number)| println!("{}. {}", number, item));
@@ -27,7 +27,7 @@ fn main() {
     //price
     let crypto_price_selector = scraper::Selector::parse("div.css-b1ilzc").unwrap();
     let cryptoprice = document.select(&crypto_price_selector).map(|x| x.inner_html());
-    println!("Price \n");
+    println!("\n PRICE \n");
     cryptoprice
         .zip(1..51)
         .for_each(|(item, number)| println!("{}. {}", number, item));
@@ -36,7 +36,7 @@ fn main() {
     //change
     let crypto_change_selector = scraper::Selector::parse("td.css-1b7j986>p").unwrap();
     let cryptochange = document.select(&crypto_change_selector).map(|x| x.inner_html());
-    println!("change \n");
+    println!("\n 24H CHANGE \n");
     cryptochange
         .zip(1..51)
         .for_each(|(item, number)| println!("{}. {}", number, item));
@@ -45,7 +45,7 @@ fn main() {
     //volume
     let crypto_volume_selector = scraper::Selector::parse("td.css-1nh9lk8").unwrap();
     let cryptovolume = document.select(&crypto_volume_selector).map(|x| x.inner_html());
-    println!("volume \n");
+    println!("\n 24H VOLUME \n");
     cryptovolume
         .zip(1..51)
         .for_each(|(item, number)| println!("{}. {}", number, item));
@@ -54,7 +54,7 @@ fn main() {
     //cap
     let crypto_cap_selector = scraper::Selector::parse("td.css-1nh9lk8").unwrap();
     let cryptocap = document.select(&crypto_cap_selector).map(|x| x.inner_html());
-    println!("Cap \n");
+    println!("\N MATKET CAP \n");
     cryptocap
         .zip(1..51)
         .for_each(|(item, number)| println!("{}. {}", number, item));
