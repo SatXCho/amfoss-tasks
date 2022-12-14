45bc12f7398aa391e6f53e40d4341aa665d7a025
SatXCho <92297743+SatXCho@users.noreply.github.com>
2022-12-14 20:41:47 +0530

    yowza that's a lot of files

--- task-9/src/main.rs
+++ task-9/src/main.rs
@@ -1,4 +1,5 @@
-
+use std::fs::File;
+use std::io::prelude::*;
 extern crate reqwest;
 extern crate scraper;
 
@@ -14,52 +15,56 @@ fn main() {
     .unwrap()
     .text()
     .unwrap();
+
+    let mut file = File::create("out.csv")
+    .expect("no file");
+
     
     //name
     let document = scraper::Html::parse_document(&response);
     let crypto_name_selector = scraper::Selector::parse("div.css-ttxvk0>p").unwrap();
     let cryptoname = document.select(&crypto_name_selector).map(|x| x.inner_html());
-    println!("\n NAME \n");
+
     cryptoname
         .zip(1..51)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
+        .for_each(|(item, number)| { file.write_all(item.as_bytes()).expect("no"); });
 
     //price
     let crypto_price_selector = scraper::Selector::parse("div.css-b1ilzc").unwrap();
     let cryptoprice = document.select(&crypto_price_selector).map(|x| x.inner_html());
-    println!("\n PRICE \n");
+
     cryptoprice
         .zip(1..51)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
+        .for_each(|(item, number)| { file.write_all(item.as_bytes()).expect("no"); });
 
     //css-1b7j986
     //change
     let crypto_change_selector = scraper::Selector::parse("td.css-1b7j986>p").unwrap();
     let cryptochange = document.select(&crypto_change_selector).map(|x| x.inner_html());
-    println!("\n 24H CHANGE \n");
+
     cryptochange
         .zip(1..51)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
+        .for_each(|(item, number)| { file.write_all(item.as_bytes()).expect("no"); });
     
     //css-1nh9lk8
     //volume
     let crypto_volume_selector = scraper::Selector::parse("td.css-1nh9lk8").unwrap();
     let cryptovolume = document.select(&crypto_volume_selector).map(|x| x.inner_html());
-    println!("\n 24H VOLUME \n");
+
     cryptovolume
         .zip(1..51)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
+        .for_each(|(item, number)| { file.write_all(item.as_bytes()).expect("no"); });
 
     //css-1nh9lk8
     //cap
     let crypto_cap_selector = scraper::Selector::parse("td.css-1nh9lk8").unwrap();
     let cryptocap = document.select(&crypto_cap_selector).map(|x| x.inner_html());
-    println!("\N MATKET CAP \n");
+
     cryptocap
         .zip(1..51)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
-    
+        .for_each(|(item, number)| { file.write_all(item.as_bytes()).expect("no"); });
 
+    
 
 
 }
